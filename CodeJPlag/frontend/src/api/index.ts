d7296fc2d156be7a9ced91af7f9b9726af889397
葉羽.Gmiko <154506891+YeuGmiko@users.noreply.github.com>
2024-10-16 16:32:49 +0800

    Delete CodeJPlag directory

--- CodeJPlag/frontend/src/api/index.ts
+++
@@ -1,6 +0,0 @@
-import Axios from "axios";
-
-export const axios = Axios.create({
-    baseURL: 'http://localhost:8080/api',
-    withCredentials: true
-})
--- CodeJPlag/frontend/src/api/jplag/typing.ts
+++
@@ -1,5 +0,0 @@
-interface JPlagByFilesOption {
-    language: 'Java' | 'JavaScript' | 'TypeScript' | 'C',
-    files: File[],
-    baseCode: File | null | undefined
-}
